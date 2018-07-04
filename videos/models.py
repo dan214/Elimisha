f4770c079eb7aa13dbe623b29f568b21b95cf641
dan_the_great <danielmiji@gmail.com>
2018-07-04 20:52:28 +0300

    add quiz models

--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 
+class Quiz(models.Model):
+    name = models.CharField(max_length=200)
+    document = models.FileField(upload_to='video_folder/')
+
+    def __str__(self):
+        return self.name
+
 class Video(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length= 400)
     document = models.FileField(upload_to='video_folder/')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE,null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
