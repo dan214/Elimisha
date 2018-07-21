4fc7c6ef3f4ad884d2774a1c70395271be978a95
Daniel Irungu <eric.nzai@asandk.com>
2018-07-21 18:38:09 +0300

    add styling to add video create form

--- videos/forms.py
+++ videos/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput, FileInput,ImageField, ChoiceField, Select, ModelChoiceField
 from .models import Video
 
 class VideoForm(ModelForm):
     class Meta:
         model = Video
         fields = '__all__'
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+            'document': FileInput(attrs={'class': 'form-control-file'}),
+            'image': FileInput(attrs={'class': 'form-control-file'}),
+            'quiz': Select(attrs={'class': 'form-control'}),
+            'course': Select(attrs={'class': 'form-control'})
+        }
--- videos/static/videos/js/app.js
+++ videos/static/videos/js/app.js
@@ -3,13 +3,18 @@
 SLICK_SLIDER = {
 
     bindSlickSlider: function(){
-        $(document).ready(function(){
-            $(".watch-videos-slide").slick({
-                infinite:true,
-                slidesToShow: 3,
-                slidesToScroll: 3
+
+        var sliderVideoId = $(".watch-videos-slide");
+        if(sliderVideoId.length > 0){
+            $(document).ready(function(){
+                $(".watch-videos-slide").slick({
+                    infinite:true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                })
             })
-        })
+        }
+
     }
 }
 
