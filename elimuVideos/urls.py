469fbe2758ebb27c6e654631876d8a64b99773f4
Daniel Irungu <v-dairungu@microsoft.com>
2023-10-06 23:51:04 +0300

    fix url issue after upgrade

--- elimuVideos/urls.py
+++ elimuVideos/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path as url
 
 
 urlpatterns = [
